8357ea6bfc0ec588e2b1a79e832b872e4830e3fe
Smit Joshi <smitjoshi814@gmail.com>
2023-07-14 19:12:09 +0530

    BMI

--- practical7.py
+++ practical7.py
@@ -1,5 +1,5 @@
 # calculate BMI
-weight=int(input("Enter your Weight: "))
-height=float(input("Enter your height: "))
+weight=int(input("Enter your Weight in kgs: "))
+height=float(input("Enter your height in meters: "))
 bmi=weight/(height**2)
 print("Your BMI is:",bmi)
