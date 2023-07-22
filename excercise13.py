c43ba20139d763a9b988280528712ee755c8a8df
Smit Joshi <smitjoshi814@gmail.com>
2023-07-22 13:22:09 +0530

    hide your money

--- excercise13.py
+++ excercise13.py
@@ -1,6 +1,6 @@
-sub_list1=[1,1,1]
-sub_list2=[1,1,1]
-sub_list3=[1,1,1]
+sub_list1=['😊','😊','😊']
+sub_list2=['😊','😊','😊']
+sub_list3=['😊','😊','😊']
 main_list=[sub_list1,sub_list2,sub_list3]
 
 hide_money=int(input("Enter location to hide your money: "))
@@ -10,3 +10,5 @@ column=int(hide_money%10)
 
 row_selected=main_list[row-1]
 row_selected[column-1]='X'
+
+print(f"{sub_list1}\n{sub_list2}\n{sub_list3}")
