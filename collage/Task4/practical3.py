83312428ca9f7e2a3b1e42ade62874ba1441df64
Smit Joshi <smitjoshi814@gmail.com>
2023-08-10 17:39:23 +0530

    Practical 3

---
+++ collage/Task4/practical3.py
@@ -0,0 +1,32 @@
+
+# functions start
+def sum(num1,num2):
+    return num1+num2
+
+def minus(num1,num2):
+    return num1-num2
+
+def multiply(num1,num2):
+    return num1*num2
+
+def devide(num1,num2):
+    return num1/num2
+# functions end
+
+num1=int(input("Enter Num 1: "))
+num2=int(input("Enter num 2: "))
+ans=0
+operator=input("Which Operation you want to perform? [+] [-][*][/] : ")
+
+match operator:
+    case '+': 
+        ans=sum(num1, num2)
+    case '-':
+        ans=minus(num1, num2) 
+    case '*':
+        ans=multiply(num1,num2)
+    case '/':
+        ans=devide(num1, num2)
+    case default: print("Invalid choice")
+
+print(f"The Answer is: {ans}")
