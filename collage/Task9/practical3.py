5fdf057f67489e1ed6bbf829b296124479906bc5
Smit Joshi <smitjoshi814@gmail.com>
2023-10-26 09:59:35 +0530

    Task 9

---
+++ collage/Task9/practical3.py
@@ -0,0 +1,15 @@
+N=int(input("Enter N: "))
+
+try:
+    if N <= 0:
+        raise Exception("N can't be Negative or Zero")
+    else:
+        number=1
+        sum=0
+        while N>0:
+            sum=sum+(number*number)
+            number=number+2
+            N=N-1
+        print("The Solution is",sum)
+finally:
+    print("Execution Completed")
